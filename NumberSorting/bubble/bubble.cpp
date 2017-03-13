af391d2cbd7b8e7ce108112bc6f151170389cf44
paulkowa <paulkowa@buffalo.edu>
2017-03-13 16:25:11 -0400

    Added bubble and bi-driectional bubble sort

---
+++ NumberSorting/bubble/bubble.cpp
@@ -0,0 +1,85 @@
+// Bubble sort and Bi-Directional Bubble sort
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <string>
+
+void fill_array(std::vector<int> &input, int seed = 56) {
+    srand(seed);
+    for(int i = 0; i < 100; i++) {
+        input.push_back(rand() % 100);
+    }
+}
+
+bool is_sorted(std::vector<int> &input) {
+    bool sorted = true;
+    for(int i = 0; i + 1 < input.size(); i++) {
+        if(input[i] > input[i + 1])
+            sorted = false;
+    }
+
+    return sorted;
+}
+
+void bubble_sort(std::vector<int> &input) {
+    int k = input.size();
+
+    while(!is_sorted(input)) {
+        for(int i = 0; i + 1 < k; i++) {
+            if(input[i] > input[i + 1]){
+                int temp = input[i + 1];
+                input[i + 1] = input[i];
+                input[i] = temp;
+            }
+        }
+        k--;
+    }
+}
+
+void bi_dir_bubble(std::vector<int> &input) {
+    int k = input.size();
+    int s = 0;
+
+    while(!is_sorted(input)) {
+        for(int i = 0; i + 1 < k; i++) {
+            if(input[i] > input[i + 1]){
+                int temp = input[i + 1];
+                input[i + 1] = input[i];
+                input[i] = temp;
+            }
+        }
+
+        for(int i = input.size() - 1; i - 1 > s; i--) {
+            if(input[i] < input[i - 1]){
+                int temp = input[i - 1];
+                input[i - 1] = input[i];
+                input[i] = temp;
+            }
+        }
+
+        k--;
+        s++;
+    }
+}
+
+int main(int argc, char *argv[]){
+    std::vector<int> numbers;
+    fill_array(numbers);
+
+     for(int i = 0; i < numbers.size(); i++){
+        std::cout << numbers[i] << " ";
+    } std::cout << std::endl;
+
+    // bubble_sort(numbers);
+    bi_dir_bubble(numbers);
+
+    for(int i = 0; i < numbers.size(); i++){
+        std::cout << numbers[i] << " ";
+    } std::cout << std::endl;
+
+    return 0;
+
+}
